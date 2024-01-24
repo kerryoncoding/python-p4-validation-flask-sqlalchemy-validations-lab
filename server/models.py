64fa9ac09062bb956d4abd8dd801aca131ae43e9
Kerry Guarino <kerry.guarino@gmail.com>
2024-01-24 08:19:14 -0500

    all but one test passes

--- server/models.py
+++ server/models.py
@@ -11,7 +11,24 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+   
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        # name must be unique
+        if not name:
+            raise ValueError("Name field is required.")
+        author = db.session.query(Author.id).filter_by(name=name).first()
+        if author is not None:
+            raise ValueError("Name must be unique.")
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError("Phone number must be 10 digits.")
+        return phone_number
+
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,7 +45,38 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('content')
+    def validate_content(self, key, content):
+        # min post length of 250 chars
+        if len(content) < 250:
+            raise ValueError("Content must be a minimum of 250 Characters")
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        # max post length of 250 chars
+        if len(summary) > 250:
+            raise ValueError("summary must be a maximum of 250 Characters")
+        return summary
+    
 
+    @validates('category')
+    def validate_category(self, key, category):
+        # category is 'Fiction' or 'Non-Fiction' only
+        if (category != "Fiction") and (category != 'Non-Fiction'):
+            raise ValueError("category must be 'Fiction' or Non-Fiction")
+        return category
+
+    @validates('title')
+    def validate_title(self, key, title):
+        # title contains "Won't Believe" "Secret" "Top" or "Guess"
+        if not title:
+            raise ValueError("title field is required")
+        key_words = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in key_words):
+            raise ValueError("title must contain 'Won\'t Believe', 'Secret', 'Top', or 'Guess'" )
+        return title
+    
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
